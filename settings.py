0894e9560f614db2b89475f293391e06460f53ec
Benson Njogu <bentric@yahoo.com>
2016-07-01 09:47:29 +0300

    fixed import error

--- settings.py
+++ settings.py
@@ -107,7 +107,7 @@ SESSION_CONFIG_DEFAULTS = {
 
 try:
     from local_settings import *
-except FileNotFoundError:
+except:
     # if on server import prod settings
     from local_settings_prod import *
 
