cd3abf5ec4fca5b6e91cddc7537c7cb7bb3b26a3
Joseph <joseph.kimani@busaracenter.org>
2017-08-23 12:57:04 +0300

    changes on the production game

--- production/views.py
+++ production/views.py
@@ -13,61 +13,177 @@ class Introduction(Page):
         return self.subsession.round_number == 1
 
 
-class PracticeSlider(Page):
+class PracticeSliderOne(Page):
     def is_displayed(self):
         return self.subsession.round_number == 1
 
     form_model = models.Player
-    form_fields = ['practice_slider_value_one', 'practice_slider_value_two']
+    form_fields = ['practice_slider_value_one']
 
     def vars_for_template(self):
         return {
             'practice_random_slider_value_one': self.player.practice_random_slider_value_one,
+        }
+
+
+class PracticeSliderTwo(Page):
+    def is_displayed(self):
+        return self.subsession.round_number == 1
+
+    form_model = models.Player
+    form_fields = ['practice_slider_value_two']
+
+    def vars_for_template(self):
+        return {
             'practice_random_slider_value_two': self.player.practice_random_slider_value_two,
         }
 
-    timeout_seconds = 300
-
 
 class WaitPage(WaitPage):
-
     def after_all_players_arrive(self):
         pass
 
 
-class Slider(Page):
+class SliderOne(Page):
     form_model = models.Player
-    form_fields = ['player_slider_value_one', 'player_slider_value_two', 'player_slider_value_three', 'player_slider_value_four', 'player_slider_value_five', 'player_slider_value_six', 'player_slider_value_seven', 'player_slider_value_eight', 'player_slider_value_nine', 'player_slider_value_ten']
+    form_fields = ['player_slider_value_one']
 
     def vars_for_template(self):
         return {
-            'random_slider_value_one': self.player.random_slider_value_one,
+            'random_slider_value_one': self.player.random_slider_value_one
+        }
+
+    timeout_seconds = 180
+
+
+class SliderTwo(Page):
+    form_model = models.Player
+    form_fields = ['player_slider_value_two']
+
+    def vars_for_template(self):
+        return {
             'random_slider_value_two': self.player.random_slider_value_two,
-            'random_slider_value_three': self.player.random_slider_value_three,
-            'random_slider_value_four': self.player.random_slider_value_four,
-            'random_slider_value_five': self.player.random_slider_value_five,
-            'random_slider_value_six': self.player.random_slider_value_six,
+        }
+
+    timeout_seconds = 180
+
+
+class SliderThree(Page):
+    form_model = models.Player
+    form_fields = ['player_slider_value_three']
+
+    def vars_for_template(self):
+        return {
+            'random_slider_value_three': self.player.random_slider_value_three
+        }
+
+    timeout_seconds = 180
+
+
+class SliderFour(Page):
+    form_model = models.Player
+    form_fields = ['player_slider_value_four']
+
+    def vars_for_template(self):
+        return {
+            'random_slider_value_four': self.player.random_slider_value_four
+        }
+
+    timeout_seconds = 180
+
+
+class SliderFive(Page):
+    form_model = models.Player
+    form_fields = ['player_slider_value_five']
+
+    def vars_for_template(self):
+        return {
+            'random_slider_value_five': self.player.random_slider_value_five
+        }
+
+    timeout_seconds = 180
+
+
+class SliderSix(Page):
+    form_model = models.Player
+    form_fields = ['player_slider_value_six']
+
+    def vars_for_template(self):
+        return {
+            'random_slider_value_six': self.player.random_slider_value_six
+        }
+
+    timeout_seconds = 180
+
+
+class SliderSeven(Page):
+    form_model = models.Player
+    form_fields = ['player_slider_value_seven']
+
+    def vars_for_template(self):
+        return {
             'random_slider_value_seven': self.player.random_slider_value_seven,
+        }
+
+    timeout_seconds = 180
+
+
+class SliderEight(Page):
+    form_model = models.Player
+    form_fields = ['player_slider_value_eight']
+
+    def vars_for_template(self):
+        return {
             'random_slider_value_eight': self.player.random_slider_value_eight,
+        }
+
+    timeout_seconds = 180
+
+
+class SliderNine(Page):
+    form_model = models.Player
+    form_fields = ['player_slider_value_nine']
+
+    def vars_for_template(self):
+        return {
             'random_slider_value_nine': self.player.random_slider_value_nine,
+        }
+
+    timeout_seconds = 180
+
+
+class SliderTen(Page):
+    form_model = models.Player
+    form_fields = ['player_slider_value_ten']
+
+    def vars_for_template(self):
+        return {
             'random_slider_value_ten': self.player.random_slider_value_ten,
         }
 
-    timeout_seconds = 300
+    timeout_seconds = 180
 
 
 class ResultsWaitPage(WaitPage):
-
     def after_all_players_arrive(self):
         self.group.set_correct_sliders()
         self.group.set_sliders_total()
 
 
-
 page_sequence = [
     Introduction,
-    PracticeSlider,
+    PracticeSliderOne,
+    PracticeSliderTwo,
     WaitPage,
-    Slider,
+    SliderOne,
+    SliderTwo,
+    SliderThree,
+    SliderFour,
+    SliderFive,
+    SliderSix,
+    SliderSeven,
+    SliderEight,
+    SliderNine,
+    SliderTen,
     ResultsWaitPage,
 ]
