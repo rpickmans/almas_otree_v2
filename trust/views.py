51b8463344e71449f61bb7415824ca4499fd5e05
jobm <jobappdeveloper@outlook.com>
2017-09-12 08:52:47 +0300

    cant send o back in trust game

--- trust/views.py
+++ trust/views.py
@@ -28,37 +28,17 @@ class ShuffleWaitPage(WaitPage):
 
     def after_all_players_arrive(self):
         self.subsession.group_randomly()
-        # # sort players by amount sent
-        # sorted_players = sorted(
-        #     self.subsession.get_players(),
-        #     key=lambda player: player.sent_amount
-        # )
-        #
-        # if len(sorted_players) == 3:
-        #     pass
-        # else:
-        #     # assuming 6, 12, 0r 18 players
-        #     median_sort = len(sorted_players) / 2
-        #     for i in sorted_players:
-        #         if sorted_players.index(i) > median_sort:
-        #             i.participant.vars["ranked"] = "high"
-        #         else:
-        #             i.participant.vars["ranked"] = "low"
-        #
-        # # chunk players into groups
-        # group_matrix = []
-        # ppg = Constants.players_per_group
-        # for i in range(0, len(sorted_players), ppg):
-        #     group_matrix.append(sorted_players[i:i+ppg])
-        #
-        # # set new groups
-        # self.subsession.set_group_matrix(group_matrix)
 
 
 class SendBack(Page):
     # both players are player 2
     form_model = models.Player
     form_fields = ['sent_back_amount']
+
+    def is_displayed(self):
+        if self.player.get_others_in_group()[0].sent_amount <= 0:
+            self.player.sent_back_amount = 0
+        return self.player.get_others_in_group()[0].sent_amount > 0
 
     def vars_for_template(self):
         amount_sent = self.player.get_others_in_group()[0].sent_amount
