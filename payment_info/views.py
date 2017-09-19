2960e41ae0b4e3c37f3b3c2a558a7645c7e88f5d
jobm <jobappdeveloper@outlook.com>
2017-09-19 10:12:08 +0300

    updating participant payoff on payment_info

--- payment_info/views.py
+++ payment_info/views.py
@@ -12,6 +12,7 @@ class Wait(WaitPage):
 
     def after_all_players_arrive(self):
         for p in self.group.get_players():
+            p.participant.payoff = p.participant.vars["carrying_payoff"] / 3.5
             p.payoff = p.participant.vars["carrying_payoff"] / 3.5
             p.total_points = p.participant.vars["carrying_payoff"]
 
